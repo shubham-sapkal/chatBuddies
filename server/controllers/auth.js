cdf13eb8c5bf58ffb7c8b26486fc827e4bfed580
shubham-sapkal <sapkal.shubham2002@gmail.com>
2023-06-01 22:10:50 +0530

    reflected server side code

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -9,53 +9,10 @@ const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
 
-const login = async (req, res) => {
-
-    try{
-
-        const {userName, password} = req.body;
-
-        const serverClient = connect(api_key, api_secret, app_id);
-        const client = StreamChat.getInstance(api_key, api_secret);
-
-        const {users} = await client.queryUsers({name: userName});
-
-        if(!users.length) return res.status(400).json({message: 'User Not Found!'})
-
-        const success = await bcrypt.compare(password, users[0].hashedPassword);
-
-        const token = serverClient.createUserToken(users[0].id);
-
-
-        if(success)
-        {
-            res.status(200).json({
-                token, 
-                fullName: users[0].fullName,
-                userName,
-                userId: users[0].id
-            });
-        }
-        else{
-            res.status(500).json({message: 'Incorrect Password!'});
-        }
-
-    } catch(error)
-    {
-        console.log(error);
-
-        res.status(500).json({message: error});
-    }
-
-};
-
 const signup = async (req, res) => {
+    try {
+        const { fullName, username, password, phoneNumber } = req.body;
 
-    try{
-
-        const {fullName, userName, password, phoneNumber} = req.body;
-
-        // generating random user id
         const userId = crypto.randomBytes(16).toString('hex');
 
         const serverClient = connect(api_key, api_secret, app_id);
@@ -64,15 +21,39 @@ const signup = async (req, res) => {
 
         const token = serverClient.createUserToken(userId);
 
-        res.status(200).json({token, fullName, userName, userId, hashedPassword, phoneNumber});
-
-    } catch(error)
-    {
+        res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber });
+    } catch (error) {
         console.log(error);
 
-        res.status(500).json({message: error});
+        res.status(500).json({ message: error });
     }
+};
 
+const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret);
+
+        const { users } = await client.queryUsers({ name: username });
+
+        if(!users.length) return res.status(400).json({ message: 'User not found' });
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+        const token = serverClient.createUserToken(users[0].id);
+
+        if(success) {
+            res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id});
+        } else {
+            res.status(500).json({ message: 'Incorrect password' });
+        }
+    } catch (error) {ads
+        console.log(error);
+
+        res.status(500).json({ message: error });
+    }
 };
 
 module.exports = { signup, login }
--- server/index.js
+++ server/index.js
@@ -6,23 +6,45 @@ const authRoutes = require("./routes/auth.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// call enviroment 
 require('dotenv').config();
 
-// middleware
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
+
 app.use(cors());
-
 app.use(express.json());
-
 app.use(express.urlencoded());
 
-
-// Route
 app.get('/', (req, res) => {
-    // res.header( "Access-Control-Allow-Origin" );
     res.send('Hello, World!');
+});
+
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body;
+
+    if(type === 'message.new') {
+        members
+            .filter((member) => member.user_id !== sender.id)
+            .forEach(({ user }) => {
+                if(!user.online) {
+                    twilioClient.messages.create({
+                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+                        messagingServiceSid: messagingServiceSid,
+                        to: user.phoneNumber
+                    })
+                        .then(() => console.log('Message sent!'))
+                        .catch((err) => console.log(err));
+                }
+            })
+
+            return res.status(200).send('Message sent!');
+    }
+
+    return res.status(200).send('Not a new message request');
 });
 
 app.use('/auth', authRoutes);
 
-app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
